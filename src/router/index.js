8d957b78e6f85d9c07bd86c86cd23e4372051f45
rooyca <rooyca@gmail.com>
2022-07-21 09:51:56 -0500

    Heroku deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Visualizer from '../views/Visualizer.vue';
+import Settings from '../views/Settings.vue';
 
 const routes = [
     { path: '/', redirect: { name: 'settings' } },
-    { path: '/settings', name: 'settings', component: () => import('../views/Settings.vue') },
+    { path: '/settings', name: 'settings', component: Settings },
     { path: '/slides/:id', params: true, component: Visualizer },
 ];
 
